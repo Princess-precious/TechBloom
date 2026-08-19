7277750cf858ec2d9d0e99c4a08f5e140864374f
Princess <princessenemanna@gmail.com>
2026-08-19 16:40:34 +0100

    structure-update

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+/**
+    * @description      : 
+    * @author           : HP
+    * @group            : 
+    * @created          : 19/08/2026 - 16:38:46
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 19/08/2026
+    * - Author          : HP
+    * - Modification    : 
+**/
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Home Page</h1>
+    </div>
+  )
+}
