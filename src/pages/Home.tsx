876d63ee7befa83fdfe59c85a5be760aee499060
Princess <princessenemanna@gmail.com>
2026-08-22 15:41:11 +0100

    Set up Tailwind, Router and Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+/**
+    * @description      : 
+    * @author           : HP
+    * @group            : 
+    * @created          : 22/08/2026 - 14:48:27
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 22/08/2026
+    * - Author          : HP
+    * - Modification    : 
+**/
+import Navbar from "../components/Navbar";
+
+function Home(){
+  return(
+    <>
+      <Navbar/>
+    </>
+  );
+}
+
+export default Home;
