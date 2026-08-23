6d2fc50852e06ffd4b0bd1d7093e172fd2e991f3
Princess <princessenemanna@gmail.com>
2026-08-23 14:00:49 +0100

    index.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,9 +14,9 @@ import Logo from "../images/logo.png";
 
 function Navbar() {
   return(
-    <nav className="bg-red-500 p-4 flex items-center h-[80px]">
+    <nav className="bg-[#010e24] p-4 flex items-center h-[80px]">
   <div>
-    <img src={Logo} alt="logo" className=""></img>
+    <img src={Logo} alt="logo" className="w-[25px] h-[25px]"></img>
   </div>
   <div></div>
   <div></div>
