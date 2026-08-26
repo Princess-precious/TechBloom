5e3dd68746745d68a57d4dfa121b1a717480af48
Princess <princessenemanna@gmail.com>
2026-08-26 15:40:14 +0100

    Remove Learninghub route from App component

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,12 @@
     * - Modification    : 
 **/
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Learninghub from "./pages/learninghub";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/learninghub" element={<Learninghub />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
